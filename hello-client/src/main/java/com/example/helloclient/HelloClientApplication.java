6beb9de7cb595913702d348ff949d853c08f3d46
vijaygovind <vijucg@yahoo.com>
2018-09-05 10:51:46 +0530

    Initial Commit from Vijay

---
+++ hello-client/src/main/java/com/example/helloclient/HelloClientApplication.java
@@ -0,0 +1,37 @@
+package com.example.helloclient;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+
+@SpringBootApplication
+public class HelloClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloClientApplication.class, args);
+	}
+}
+	
+	@Configuration
+	
+	class Config{
+		
+		@LoadBalanced
+		@Bean
+		public RestTemplate restTemplate()
+		{
+			return new RestTemplate();
+		}
+		
+	
+	}
+	
+
---
+++ hello-client/src/main/java/com/example/helloclient/HelloResource.java
@@ -0,0 +1,24 @@
+package com.example.helloclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/rest/hello/client")
+
+public class HelloResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping
+	public String hello()
+	{
+		String url ="http://hello-server/rest/hello/server";
+		return restTemplate.getForObject(url,String.class);
+	}
+
+}
